9ddbc6e520cdbc2c6307941085e29f64cd8bbd0f
jeanfabre <contact@fabrejean.net>
2015-12-08 16:30:02 +0300

    - events now broadcasting for each eyes when valid/invalid - New scene using uGui for eyes positioning

--- Assets/PlayMaker Tobii Eyex/Components/PlayMakerEyexEyePositionDataProxy.cs
+++ Assets/PlayMaker Tobii Eyex/Components/PlayMakerEyexEyePositionDataProxy.cs
@@ -8,7 +8,7 @@ using Tobii.EyeX.Framework;
 /// <summary>
 /// PlayMaker Proxy Component for EyePositionDataComponent.
 /// </summary>
-[AddComponentMenu("PlayMaker/Addons/Tobii EyeX/Eye Position Data Proxy")]
+//[AddComponentMenu("PlayMaker/Addons/Tobii EyeX/Eye Position Data Proxy")]
 public class PlayMakerEyexEyePositionDataProxy : MonoBehaviour {
 
 	public FixationDataMode fixationDataMode;
@@ -23,11 +23,21 @@ public class PlayMakerEyexEyePositionDataProxy : MonoBehaviour {
 
 	public static PlayMakerEyexEyePositionDataProxy Instance;
 
+	public static string LeftEyeValidEventName = "TOBII / EYEX / LEFT EYE VALID";
+	public static string LeftEyeInValidEventName = "TOBII / EYEX / LEFT EYE INVALID";
+	public static string RightEyeValidEventName = "TOBII / EYEX / RIGHT EYE VALID";
+	public static string RightEyeInValidEventName = "TOBII / EYEX / RIGHT EYE INVALID";
+
+
+	bool LastLeftEyeValidState = false;
+	bool LastRightEyeValidState = false;
+
 	protected void Awake()
 	{
 		Instance = this;
 		_eyexHost = EyeXHost.GetInstance();
 		_dataProvider = _eyexHost.GetEyePositionDataProvider();
+
 	}
 	
 	protected void OnEnable()
@@ -43,5 +53,20 @@ public class PlayMakerEyexEyePositionDataProxy : MonoBehaviour {
 	protected void Update()
 	{
 		LastEyePosition = _dataProvider.Last;
+
+		if (LastEyePosition.LeftEye.IsValid != LastLeftEyeValidState)
+		{
+			LastLeftEyeValidState = LastEyePosition.LeftEye.IsValid;
+		
+			PlayMakerFSM.BroadcastEvent(LastLeftEyeValidState?LeftEyeValidEventName:LeftEyeInValidEventName);
+		}
+
+		if (LastEyePosition.RightEye.IsValid != LastRightEyeValidState)
+		{
+			LastRightEyeValidState = LastEyePosition.RightEye.IsValid;
+			
+			PlayMakerFSM.BroadcastEvent(LastRightEyeValidState?RightEyeValidEventName:RightEyeInValidEventName);
+		}
+
 	}
 }
